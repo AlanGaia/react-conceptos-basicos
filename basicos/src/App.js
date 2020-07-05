7c675ad9e6766fb62fe8dab3cd9cd7fa05777175
Alan <devaliant94@gmail.com>
2020-07-05 01:05:01 -0300

    adding useState Hooks

--- basicos/src/App.js
+++ basicos/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
+  //Get Product List
+  const [products, setProducts] = useState([
+    { id: 1, name: "Shirt ReactJS", price: 50,},
+    { id: 2, name: "NodeJS Pants", price: 40,},
+    { id: 3, name: "Jacket JavaScript", price: 120,},
+    { id: 4, name: "Shoes VueJS", price: 20,},
+  ]);
 
- //obtener el año
- const year = new Date().getFullYear(); 
+  //Get the actual year
+  const year = new Date().getFullYear();
+
   return (
     <>
-      <Header title='Tienda virtual' />
+      <Header title="Tienda virtual" />
 
       <Footer year={year} />
     </>
